49d6a0753a7a4581910d4f035c5ac5cff5ab1f20
tritainguyen98 <56943599+tritainguyen98@users.noreply.github.com>
2019-12-04 11:14:53 -0800

    added histograms

---
+++ extractWeights.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+
+from LeNet import Net
+
+def listWeights(net):
+            w = torch.flatten(list(net.conv1.parameters())[0]).detach().numpy()
+            print(w)
+            print("Min: {}\nMax: {}\nStd: {}\nSize:{}".format(np.min(w), np.max(w), np.std(w), w.size))
+            plt.hist(w)
+            plt.show()
+            
+            w = torch.flatten(list(net.conv2.parameters())[0]).detach().numpy()
+            print(w)
+            print("Min: {}\nMax: {}\nStd: {}\nSize:{}".format(np.min(w), np.max(w), np.std(w), w.size))
+            plt.hist(w)
+            plt.show()
+
+
+            w = torch.flatten(list(net.fc1.parameters())[0]).detach().numpy()
+            print(w)
+            print("Min: {}\nMax: {}\nStd: {}\nSize:{}".format(np.min(w), np.max(w), np.std(w), w.size))
+            plt.hist(w)
+            plt.show()
+
+            w = torch.flatten(list(net.fc2.parameters())[0]).detach().numpy()
+            print(w)
+            print("Min: {}\nMax: {}\nStd: {}\nSize:{}".format(np.min(w), np.max(w), np.std(w), w.size))
+            plt.hist(w)
+            plt.show()
+
+            w = torch.flatten(list(net.fc3.parameters())[0]).detach().numpy()
+            print(w)
+            print("Min: {}\nMax: {}\nStd: {}\nSize:{}".format(np.min(w), np.max(w), np.std(w), w.size))
+            plt.hist(w)
+            plt.show()
